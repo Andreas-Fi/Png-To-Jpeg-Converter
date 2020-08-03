0c3e311c6c51fd3fa155ca55806b8b8de65fa3d6
Andreas-Fi <andreas.pujol@gmail.com>
2020-08-03 19:29:39 +0300

    Update

--- PngToJpegConverter/Program.cs
+++ PngToJpegConverter/Program.cs
@@ -13,16 +13,44 @@ namespace PngToJpegConverter
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\Andreas\OneDrive\Wallpapers";
+            string path = "";
+            if (args.Count() >= 1)
+            {
+                if (args.Contains("-h") || args.Contains("-?"))
+                {
+                    Console.WriteLine("Usage: PngToJpegConverter -h [Input path]\n" +
+                        "\nOptions:" +
+                        "\n\t-h\t\tDisplays the help");
+                    return;
+                }
+                if (args.Count() == 1 )
+                {
+                    path = args[0];
+                }
+            }
+            else
+            {
+                Console.Write("Full path: ");
+                path = Console.ReadLine();
+            }
             IEnumerable<string> files = Directory.EnumerateFiles(path, "*.png");
+            string folder = path.Substring(path.LastIndexOf('\\') + 1);
             Console.WriteLine("{0} files found.", files.Count());
+
+            if (!Directory.Exists(@"C:\Users\Andreas\Desktop\" + folder)) 
+            {
+                Directory.CreateDirectory(@"C:\Users\Andreas\Desktop\" + folder);
+            }
+
             foreach (var item in files)
             {
                 Image image = Image.FromFile(item);
                 string fileName = item.Substring(item.LastIndexOf('\\') +1, item.LastIndexOf('.')-1 - item.LastIndexOf('\\'));
-                string newFile = @"C:\Users\Andreas\Desktop\pics\" + fileName + ".jpeg";
+                string newFile = path + "\\" + fileName + ".jpeg";
+                File.Copy(item, @"C:\Users\Andreas\Desktop\" + folder + "\\" + fileName + ".png");
                 image.Save(newFile, ImageFormat.Jpeg);
                 image.Dispose();
+                File.Delete(item);
             }
             Console.WriteLine("Conversion succeeded.");
             Console.ReadLine();
